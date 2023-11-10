adcc1a92d1b813624f0b18b94b8f852a6b86e9a8
bubsworth <bubbard96@hotmail.com>
2023-11-10 16:08:26 +0000

    Configuring event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import TabButton from './components/TabButton.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
-  function handleSelect() {
-    console.log('Hello world-selected');
+  let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
   }
 
   return (
@@ -28,11 +30,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
